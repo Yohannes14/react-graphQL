45b3ba2841075177c71206d8df777dcac85c85fd
Yohannes14 <yohansdegu.2010@gmail.com>
2023-07-30 22:15:13 +0300

    use character

---
+++ src/pages/Character.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { useCharacter } from "../hooks/useCharacter";
+import "./Character.css";
+
+const Character = () => {
+  const { data, error, loading } = useCharacter(2);
+  if (loading) return <h1>Loading....</h1>;
+  if (error) return <h1> something went wrong</h1>;
+
+  
+  return (
+    <div className="Character">
+      <img src={data.character.image} alt={data.character.image} width={750} />
+      <div className="Character-content">
+        <h1>{data.character.name}</h1>
+        <p>{data.character.gender}</p>
+        <div className="Character-episonde">
+          {data.character?.episode?.map((episode) => {
+            return (
+              <div>
+                {episode.name} -{episode.episode}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Character;
